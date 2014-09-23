18e544be867e50800be0372fe686a9d170bdbd5e
GermainZ <germanosz@gmail.com>
2014-09-23 19:26:02 +0300

    Add preference to hide icon from launcher

--- src/com/germainz/immersivemodedontpanic/Preferences.java
+++ src/com/germainz/immersivemodedontpanic/Preferences.java
@@ -1,7 +1,10 @@
 package com.germainz.immersivemodedontpanic;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 public class Preferences extends Activity {
@@ -14,7 +17,7 @@ public class Preferences extends Activity {
 
     }
 
-    private static class PrefsFragment extends PreferenceFragment {
+    public static class PrefsFragment extends PreferenceFragment {
         @SuppressWarnings("deprecation")
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,20 @@ public class Preferences extends Activity {
 
             getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
             addPreferencesFromResource(R.xml.prefs);
-       }
+
+            Preference showAppIcon = findPreference("pref_show_app_icon");
+            showAppIcon.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    PackageManager packageManager = getActivity().getPackageManager();
+                    int state = (Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                    ComponentName aliasName = new ComponentName(getActivity(),
+                            "com.germainz.immersivemodedontpanic.Preferences-Alias");
+                    packageManager.setComponentEnabledSetting(aliasName, state, PackageManager.DONT_KILL_APP);
+                    return true;
+                }
+            });
+        }
     }
 }
